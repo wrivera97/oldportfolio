c5e23b7b2585eecfcdf214ef6218c0d11e223a80
Joel <wellin12.jr0497c@gmail.com>
2020-11-20 19:04:53 -0500

    add phpemailer

--- root/contacme.php
+++ root/contacme.php
@@ -1,8 +1,38 @@
 <?php
-if ($_POST) {
-    $name="";
-    $phone="";
-    $email="";
-    $message="";  
-}
+//require './vendor/autoload.php';
+//use PHPMailer\PHPMailer\PHPMailer;
+//Create a new PHPMailer instance
+//$mail = new PHPMailer();
+//$mail->IsSMTP();
+   //reset variable to null.
+   $name = $email  = $message = $to = $headers = $msjEmail = NULL;
+
+   if (isset($_POST['submit'])) {
+      //get data from form
+      $name = $_POST['name'];
+      $email = $_POST['email'];
+      $phone = $_POST['phone'];   
+      $message = $_POST['message'];
+      $to = 'wellin12.jr0497c@gmail.com';
+
+      //headers
+      $headers = 'From: Your name <info@address.com>' . "\r\n";
+      $headers .= "Content-type: text/html; charset=utf-8";
+
+      //body email.
+      $msjEmail = "Nombre: " . $name;
+      $msjEmail .= "\r\n";
+      $msjEmail .= "Email: " . $email;
+      $msjEmail .= "\r\n";
+      $msjEmail .= "Telefono: " . $phone;
+      $msjEmail .= "\r\n";
+      $msjEmail .= "Mensaje: " . $message;
+      $msjEmail .= "\r\n";
+        //conditon before sent message
+    if (mail($to, $message, $msjEmail, $headers)) {
+         echo "okkkkk";
+    } else {
+         echo "error";
+    }
+  }
 ?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'b237ac3215ac73d24013b10e5d6d5d2ee1d69bca',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b237ac3215ac73d24013b10e5d6d5d2ee1d69bca',
+    ),
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v6.1.8',
+      'version' => '6.1.8.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '917ab212fa00dc6eacbb26e8bc387ebe40993bc1',
+    ),
+  ),
+);
